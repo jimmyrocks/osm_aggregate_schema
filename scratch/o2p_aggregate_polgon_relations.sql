6503c8bd51e4a3aec6913d85ba2977b0547802cc
Jim McAndrew <jim@loc8.us>
2013-12-30 18:06:45 -0700

    Update o2p_aggregate_polgon_relations.sql

--- scratch/o2p_aggregate_polgon_relations.sql
+++ scratch/o2p_aggregate_polgon_relations.sql
@@ -11,47 +11,35 @@ SELECT
   array_agg(polygon) polygons
 FROM (
   SELECT
-    CASE
+    st_asgeojson(CASE
       WHEN holes[1] IS NULL THEN st_makepolygon(shell)
       ELSE st_makepolygon(shell, holes)
-    END polygon
+    END) polygon
   FROM (
     SELECT
       outside.line AS shell,
-      array_agg(inside.line) AS holes,
-      outside.seq
+      array_agg(inside.line) AS holes
     FROM (
       SELECT
-        o2p_calculate_nodes_to_line(ways.nodes) AS line,
-        relation_members.member_role AS ROLE,
-        relation_members.sequence_id AS seq
+        geom as line,
+        role
       FROM
-        relation_members JOIN ways
-        ON relation_members.member_id = ways.id
+        (SELECT unnest(geom) as geom, unnest(role) as role from o2p_aggregate_relation(3351325)) z
       WHERE
-        relation_members.relation_id = v_rel_id AND
-        relation_members.member_type = 'W' AND
-        relation_members.member_role != 'inner' AND
-        ways.nodes[1] = ways.nodes[array_length(ways.nodes,1)]
+        role != 'inner' AND
+        ST_IsClosed(geom)
     ) outside LEFT OUTER JOIN (
-       SELECT
-         o2p_calculate_nodes_to_line(ways.nodes) AS line,
-         relation_members.member_role AS ROLE,
-         relation_members.sequence_id AS seq
-        FROM
-          relation_members JOIN ways
-          ON relation_members.member_id = ways.id
-        WHERE
-          relation_members.relation_id = v_rel_id AND
-          relation_members.member_type = 'W' AND
-          relation_members.member_role = 'inner' AND
-          ways.nodes[1] = ways.nodes[array_length(ways.nodes,1)]
+      SELECT
+        geom as line,
+        role
+      FROM
+        (SELECT unnest(geom) as geom, unnest(role) as role from o2p_aggregate_relation(3351325)) x
+      WHERE
+        role = 'inner' AND
+        ST_IsClosed(geom)
     ) inside ON ST_CONTAINS(st_makepolygon(outside.line), inside.line)
   GROUP BY
-    outside.line,
-    outside.seq) polys
-  ORDER BY
-    seq
+    outside.line) polys
 ) poly_array
 INTO
   v_polygons;
