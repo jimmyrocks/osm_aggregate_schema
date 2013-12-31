f33015922c29aac316382b77a5712ffa62ce1ef9
Jim McAndrew <jim@loc8.us>
2013-12-31 14:09:36 -0700

    Update and rename view_road_example.sql to view_roads_example.sql

---
+++ scratch/view_roads_example.sql
@@ -0,0 +1,85 @@
+-- roads Example view
+
+-- useful regex
+-- exist\((.+?), '(.+?)'\).+
+-- $1 -> '$2' as "$2",
+
+CREATE OR REPLACE VIEW planet_osm_roads_view AS
+SELECT 
+  osm_id,
+-------------
+"access",
+"addr:housename",
+"addr:housenumber",
+"addr:interpolation",
+"admin_level",
+"aerialway",
+"aeroway",
+"amenity",
+"area",
+"barrier",
+"bicycle",
+"brand",
+"bridge",
+"boundary",
+"building",
+"construction",
+"covered",
+"culvert",
+"cutting",
+"denomination",
+"disused",
+"embankment",
+"foot",
+"generator:source",
+"harbour",
+"highway",
+"historic",
+"horse",
+"intermittent",
+"junction",
+"landuse",
+"layer",
+"leisure",
+"lock",
+"man_made",
+"military",
+"motorcar",
+"name",
+"natural",
+"office",
+"oneway",
+"operator",
+"place",
+"population",
+"power",
+"power_source",
+"public_transport",
+"railway",
+"ref",
+"religion",
+"route",
+"service",
+"shop",
+"sport",
+"surface",
+"toll",
+"tourism",
+"tower:type",
+"tracktype",
+"tunnel",
+"water",
+"waterway",
+"wetland",
+"width",
+"wood",
+---------------
+  z_order,
+  way_area,
+  way
+FROM
+  planet_osm_line_view
+WHERE
+highway IS NOT NULL OR
+railway IS NOT NULL OR
+boundary IS NOT NULL;
